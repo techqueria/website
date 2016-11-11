fb188b76f56aa38d57742b8a3e7418d3d00eb2f3
David Silva <contacto@dvidsilva.com>
2016-11-11 15:34:03 -0800

    added offline support

---
+++ sw.js
@@ -0,0 +1,55 @@
+const VERSION = "1.0.4";
+
+const cacheName = 'techqueria';
+
+console.log(`installing sw.js`);
+
+const filesToCache = [
+    '/images/logo.png',
+    '/css/main.css',
+    '/',
+    '/index.html',
+    '/images/icons/icon-logo-techqueria-128x128.png',
+    '/images/icons/icon-logo-techqueria-144x144.png',
+    '/images/icons/icon-logo-techqueria-152x152.png',
+    '/images/icons/icon-logo-techqueria-192x192.png',
+    '/images/icons/icon-logo-techqueria-256x256.png'
+ ];
+
+this.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+ 
+this.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+this.addEventListener('fetch', function(event) {
+  var response;
+  event.respondWith(caches.match(event.request).catch(function() {
+    return fetch(event.request);
+  }).then(function(r) {
+    response = r;
+    caches.open('v1').then(function(cache) {
+      cache.put(event.request, response);
+    });
+    return response.clone();
+  }));
+});
