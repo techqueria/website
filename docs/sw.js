b686e117c4344fd113e255a18a202b675b4b1875
David Silva <contacto@dvidsilva.com>
2016-12-06 17:02:24 -0800

    trying new version of service worker

--- docs/sw.js
+++ docs/sw.js
@@ -1,15 +1,15 @@
-const VERSION = "2.0.7";
+const VERSION = "3.0.1";
 
-const cacheName = 'techqueria-org';
+// const cacheName = 'techqueria-org';
+const APP_CACHE_NAME = 'techqueria-org-app';
+const STATIC_CACHE_NAME = 'techqueria-org-static';
 
 console.log(`installing sw.js`);
 
-const filesToCache = [
+const CACHE_STATIC = [
     '/images/logo.png',
     '/images/logo-mid.png',
     '/sass/main.css',
-    '/',
-    '/index.html',
     '/images/icons/icon-logo-techqueria-128x128.png',
     '/images/icons/icon-logo-techqueria-144x144.png',
     '/images/icons/icon-logo-techqueria-152x152.png',
@@ -21,6 +21,11 @@ const filesToCache = [
     '/images/t-shirt-1.png',
     '/images/t-shirt-2.png',
     '/images/t-shirt-3.png',    
+ ];
+
+ const CACHE_APP = [
+    '/',
+    '/index.html',
     '/about/',
     '/contact/',
     '/events/',
@@ -29,42 +34,54 @@ const filesToCache = [
     '/welcome-to-slack/',
  ];
 
-this.addEventListener('install', function (e) {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
-    })
-  );
+self.addEventListener('install',function(e){
+    e.waitUntil(
+        Promise.all([
+            caches.open(STATIC_CACHE_NAME),
+            caches.open(APP_CACHE_NAME),
+            self.skipWaiting()
+          ]).then(function(storage){
+            var static_cache = storage[0];
+            var app_cache = storage[1];
+            return Promise.all([
+              static_cache.addAll(CACHE_STATIC),
+              app_cache.addAll(CACHE_APP)]);
+        })
+    );
 });
- 
-this.addEventListener('activate',function(e) {
-  console.log('[ServiceWorker] Activate');
-  e.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-  return self.clients.claim();
+
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+        Promise.all([
+            self.clients.claim(),
+            caches.keys().then(function(cacheNames) {
+                return Promise.all(
+                    cacheNames.map(function(cacheName) {
+                        if (cacheName !== APP_CACHE_NAME && cacheName !== STATIC_CACHE_NAME) {
+                            console.log('deleting',cacheName);
+                            return caches.delete(cacheName);
+                        }
+                    })
+                );
+            })
+        ])
+    );
 });
 
 this.addEventListener('fetch', function(event) {
   var response;
-  event.respondWith(caches.match(event.request).then(function (match) {
-    return match || fetch(event.request);
-  }).catch(function() {
-    return fetch(event.request);
-  }).then(function(r) {
-    response = r;
-    caches.open(cacheName).then(function(cache) {
-      cache.put(event.request, response);
-    });
-    return response.clone();
-  }));
+  event.respondWith(caches.match(event.request)
+    .then(function (match) {
+      return match || fetch(event.request);
+    }).catch(function() {
+      return fetch(event.request);
+    })
+    // .then(function(response) {
+    //   // response = r;
+    //   caches.open(cacheName).then(function(cache) {
+    //     cache.put(event.request, response);
+    //   });
+    //   return response.clone();
+    // })
+  );
 });
