84c34134dd39243a1db5068908e3cbd7c87a920b
codefactor-io <support@codefactor.io>
2019-01-01 15:50:59 +0000

    [CodeFactor] Apply fixes

--- src/js/components/lozad.js
+++ src/js/components/lozad.js
@@ -2,9 +2,9 @@ require("lozad");
 
 // Initialize library
 lozad(".lozad", {
-  load: function (el) {
+  load: function(el) {
     el.src = el.dataset.src;
-    el.onload = function () {
+    el.onload = function() {
       el.classList.add("blur");
     };
   }
