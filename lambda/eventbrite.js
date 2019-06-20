f76a732d8470ef29aabc533b2d22dae4cb3be1ea
Nicolas Botello <nbotell2@gmail.com>
2019-06-19 22:56:52 -0700

    lambda eventbrite function

---
+++ lambda/eventbrite.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+function getEventbriteEvents(status, token) {
+  return axios.get(
+    `https://www.eventbriteapi.com/v3/organizers/13735914017/events/?order_by=start_desc&status=${status}&token=${token}`,
+    {
+      headers: {Accept: "application/json"}
+    }
+  );
+}
+
+export async function handler(event, context) {
+  try {
+    const token = process.env.EVENTBRITE || "";
+    const responses = await Promise.all(
+      ["live", "completed"].map((status) => getEventbriteEvents(status, token))
+    );
+    const data = {
+      live: responses[0].data,
+      completed: responses[1].data
+    };
+    return {
+      statusCode: 200,
+      body: JSON.stringify({...data})
+    };
+  } catch (err) {
+    console.log(err); // output to netlify function log
+    return {
+      statusCode: 500,
+      body: JSON.stringify({msg: err.message})
+    };
+  }
+}
