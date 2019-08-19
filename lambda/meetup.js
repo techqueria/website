169f96d42030328dc8a7b7e43c1d10a551fd309c
Nicolas Botello <nbotell2@gmail.com>
2019-08-18 17:07:31 -0700

    fix return of images for meetup

--- lambda/meetup.js
+++ lambda/meetup.js
@@ -4,9 +4,9 @@ import axios from "axios";
 import handleResponse from "./common";
 
 function getMeetupEvents(status, token) {
+  const baseUrl = "https://api.meetup.com/techqueria";
   return axios.get(
-    `https://api.meetup.com/techqueria/events?&sign=true&photo-host=secure&status=${status}&text_format=plain&page=20&desc=true&
-    fields=featured_photo,plain_text_description`,
+    `${baseUrl}/events?&sign=true&photo-host=secure&status=${status}&text_format=plain&page=20&desc=true&fields=featured_photo,plain_text_description`,
     {
       headers: {Accept: "application/json"}
     }
